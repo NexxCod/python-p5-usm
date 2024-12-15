903e2ab0914f2fec0acac4ec0a592a1098f8cd00
NexxCod <drmsalinasv@gmail.com>
2024-12-15 16:41:53 -0300

    Calcular el precio total por producto

--- analisis_ventas.py
+++ analisis_ventas.py
@@ -5,3 +5,7 @@ import matplotlib.pyplot as plt
 archivo_csv = "ventas_productos.csv"
 df = pd.read_csv(archivo_csv)
 
+# Calcular el precio total por producto (Cantidad * Precio)
+df['Precio_Total'] = df['Cantidad'] * df['Precio']
+print("Datos con Precio Total calculado:")
+print(df)
